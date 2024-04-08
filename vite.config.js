aaed8e781b324331bb7facf2ddd94664f4e5c047
randianugrah <randianugrah79@gmail.com>
2024-04-08 20:54:50 +0700

    add base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://randianugrah.github.io/website-course/",
+  base: "/",
   plugins: [react()],
 })
